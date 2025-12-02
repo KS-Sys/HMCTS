ff8eec7858ca632e65b662e384c9d55cd96836fc
Kamaljeet Panesar <kamaljeet.panesar3@gmail.com>
2025-12-02 15:07:46 +0000

    downloaded and installed MYSQL nuget packages for program database.

--- HMCTS/Modules/DB_connector.cs
+++ HMCTS/Modules/DB_connector.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace HMCTS.Modules
 {
     internal class DB_connector
     {
-        public void GetConnected()
+        private DB_connector()
         {
 
         }
+        public MySqlConnection GetConnected { get; set; }
 
         public void OpenConnection()
         {
