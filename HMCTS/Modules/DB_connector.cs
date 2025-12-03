3479d38a6202736ef645a386fb473e7977279a22
Kamaljeet Panesar <kamaljeet.panesar3@gmail.com>
2025-12-03 14:15:31 +0000

    I have got the database working. a test was done on the main harness class with the debug method

--- HMCTS/Modules/DB_connector.cs
+++ HMCTS/Modules/DB_connector.cs
@@ -16,15 +16,43 @@ namespace HMCTS.Modules
         }
         private static DB_connector _instance = null;
         public MySqlConnection GetConnected { get; set; }
-
-        public void OpenConnection()
+        public static DB_connector Instance()
         {
+            if (_instance == null)
+                _instance = new DB_connector();
+            return _instance;
+        }
+
+        public bool OpenConnection()
+        {
+            if(GetConnected == null)
+            {
+                string conString = "Server = localhost; Port = 3306; Database = moj_db; Uid = root; Pwd = password;";
+                GetConnected = new MySqlConnection(conString);
+            }
+            
+            if (GetConnected.State != System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    GetConnected.Open();
+                    Console.WriteLine("database connected");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+     
+            }
+            return true;
 
         }
 
         public void CloseConnection()
         {
-
+            GetConnected.Close();
         }
     }
 }
--- HMCTS/Test_Harness.cs
+++ HMCTS/Test_Harness.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HMCTS.Modules;
 
 namespace HMCTS
 {
@@ -12,6 +13,14 @@ namespace HMCTS
         {
             // this is where the main code will be executed for the back_end API
             // I am using MYSQL.
+            debug();
+        }
+
+        public static void debug()
+        {
+            var dbtest = DB_connector.Instance();
+            dbtest.OpenConnection();
+            Console.Read();
         }
     }
 }
